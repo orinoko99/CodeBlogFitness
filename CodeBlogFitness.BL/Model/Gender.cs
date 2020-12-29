2b67d12b9f4f27fddeda528da99ddb34b3fa530a
orinoko99 <orinoko99@gmail.com>
2020-12-29 13:10:20 +0300

    Добавьте файлы проекта.

---
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CodeBlogFitness.BL.Model
+{
+    public class Gender
+    {
+        public string Name { get; }
+
+        public Gender(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
+            }
+        }
+    }
+}
