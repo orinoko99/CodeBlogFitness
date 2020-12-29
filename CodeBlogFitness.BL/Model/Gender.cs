6643f5c3755d22cffff5357b7e7b316c7af68602
orinoko99 <orinoko99@gmail.com>
2020-12-29 18:06:45 +0300

    Создана модель пользователья

--- CodeBlogFitness.BL/Model/Gender.cs
+++ CodeBlogFitness.BL/Model/Gender.cs
@@ -2,16 +2,33 @@
 
 namespace CodeBlogFitness.BL.Model
 {
+    /// <summary>
+    /// Пол
+    /// </summary>
     public class Gender
     {
+        /// <summary>
+        /// Название
+        /// </summary>
         public string Name { get; }
 
+        /// <summary>
+        /// Создать новый пол
+        /// </summary>
+        /// <param name="name">Имя пола</param>
         public Gender(string name)
         {
             if (string.IsNullOrWhiteSpace(name))
-            {
+            {   // проверка на валидность
                 throw new ArgumentNullException("Имя пола не может быть пустым или null", nameof(name));
             }
+
+            Name = name;
+        }
+
+        public override string ToString()
+        {
+            return Name;
         }
     }
 }
